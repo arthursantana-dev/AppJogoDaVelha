9b5948d00f55471abc9e5abc2874efb450e0661e
Usuario <Usuario@CPD02-M18>
2023-03-06 12:01:56 -0300

    Adicionar arquivos de projeto.

---
+++ AppJogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppJogoDaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "X";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            btn.IsEnabled = false;
+
+            btn.Text = vez;
+            if (vez == "X")
+            {
+                vez = "O";
+            } else
+            {
+                vez = "X";
+            }
+        }
+    }
+}
